b10adf3e9f12d6f314a9010cf8f89f03f2292795
Daniel Baud <daniel.baud@epita.fr>
2018-05-17 16:20:49 +0200

    [ENG] header

---
+++ src/engine.hh
@@ -0,0 +1,26 @@
+#include "chessboard.hh"
+#include "chessboardAdapter.hh"
+#include "listenerAdapter.hh"
+#include "network-api/server-network-api.hh"
+#include "network-api/common.hh"
+#include <string>
+
+
+class Engine
+{
+
+public:
+
+  Engine();
+
+  bool move(Position p1, Position p2);
+  bool parse(std::string path);
+  bool start_game();
+
+private:
+
+  ChessBoard chessboard;
+  ChessboardAdapter cbadapter;
+  ListenerAdapter ladapter;
+  network_api::ServerNetworkAPI net;
+};
