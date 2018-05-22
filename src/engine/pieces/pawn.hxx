612e7a5fcadc36280dc735b4eccf3182580e3f27
Daniel Baud <daniel.baud@epita.fr>
2018-05-22 18:38:07 +0200

    [PIECES] knights move handled w/o check

--- src/engine/pieces/pawn.hxx
+++ src/engine/pieces/pawn.hxx
@@ -3,7 +3,7 @@
 std::vector<Position> Pawn::get_possible_move(ChessBoard *c) const
 {
   std::vector<Position> p = std::vector<Position>();
-  if (can_move_to(c, 0, 1) == -1)
+  if (can_move_to(c, 0, 1) == 0)
     p.push_back(make_pos(0, 1));
   if (can_move_to(c, -1, 1) == 1)
     p.push_back(make_pos(-1, 1));
