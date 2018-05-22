24d5eddd46054fec0ee2a07354ee4ee4f7b3c574
Daniel Baud <daniel.baud@epita.fr>
2018-05-22 19:05:54 +0200

    [PIECE] rook moves w/o check

--- src/engine/pieces/rook.hxx
+++ src/engine/pieces/rook.hxx
@@ -2,6 +2,18 @@
 
 std::vector<Position> Rook::get_possible_move(ChessBoard *c) const
 {
-  (void)c;
-  return std::vector<Position>();
+  std::vector<Position> p = std::vector<Position>();
+  int i = 1;
+  while (can_move_to(c, i, 0) > -1)
+    p.push_back(make_pos(i++, 0));
+  i = -1;
+  while (can_move_to(c, i, 0) > -1)
+    p.push_back(make_pos(i--, 0));
+  i = 1;
+  while (can_move_to(c, 0, i) > -1)
+    p.push_back(make_pos(0, i++));
+  i = -1;
+  while (can_move_to(c, 0, i) > -1)
+    p.push_back(make_pos(0, i--));
+  return p;
 }
