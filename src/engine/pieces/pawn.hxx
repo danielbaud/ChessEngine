e4f774bb71d80d32e474fc636fa34a2c9672eebc
Daniel Baud <daniel.baud@epita.fr>
2018-05-22 18:22:20 +0200

    [PAWN] basis for a pawn move

--- src/engine/pieces/pawn.hxx
+++ src/engine/pieces/pawn.hxx
@@ -2,6 +2,12 @@
 
 std::vector<Position> Pawn::get_possible_move(ChessBoard *c) const
 {
-  (void)c;
-  return std::vector<Position>();
+  std::vector<Position> p = std::vector<Position>();
+  if (can_move_to(c, 0, 1) == -1)
+    p.push_back(make_pos(0, 1));
+  if (can_move_to(c, -1, 1) == 1)
+    p.push_back(make_pos(-1, 1));
+  if (can_move_to(c, 1, 1) == 1)
+    p.push_back(make_pos(1, 1));
+  return p;
 }
