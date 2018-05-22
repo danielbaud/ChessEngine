a7920a5cb4a5b3e628fde200b8b4da0dae2c930d
Vincent Riviere <vincent1.riviere@epita.fr>
2018-05-22 18:43:41 +0200

    [KING] Get_possible_moves implemented not yet tested

--- src/engine/pieces/king.hxx
+++ src/engine/pieces/king.hxx
@@ -2,6 +2,22 @@
 
 std::vector<Position> King::get_possible_move(ChessBoard *c) const
 {
-  (void)c;
-  return std::vector<Position>();
+  std::vector<Position> p = std::vector<Position>();
+  if (can_move_to(c, 0, 1) >= 0)
+    p.push_back(make_pos(0, 1));
+  if (can_move_to(c, 1, 1) >= 0)
+    p.push_back(make_pos(1, 1));
+  if (can_move_to(c, 1, 0) >= 0)
+    p.push_back(make_pos(1, 0));
+  if (can_move_to(c, 1, -1) >= 0)
+    p.push_back(make_pos(1, -1));
+  if (can_move_to(c, 0, -1) >= 0)
+    p.push_back(make_pos(0, -1));
+  if (can_move_to(c, -1, -1) >= 0)
+    p.push_back(make_pos(-1, -1));
+  if (can_move_to(c, -1, 0) >= 0)
+    p.push_back(make_pos(-1, 0));
+  if (can_move_to(c, -1, 0) >= 0)
+    p.push_back(make_pos(-1, 0));
+  return p;
 }
