cfad2385d98724400af9e46d222ad9af09d23408
Daniel Baud <daniel.baud@epita.fr>
2018-05-23 02:08:08 +0200

    [PIECE] pawn moves w/o check

--- src/engine/pieces/pawn.hxx
+++ src/engine/pieces/pawn.hxx
@@ -3,11 +3,19 @@
 std::vector<Position> Pawn::get_possible_move(ChessBoard *c) const
 {
   std::vector<Position> p = std::vector<Position>();
-  if (can_move_to(c, 0, 1) == 0)
-    p.push_back(make_pos(0, 1));
-  if (can_move_to(c, -1, 1) == 1)
-    p.push_back(make_pos(-1, 1));
-  if (can_move_to(c, 1, 1) == 1)
-    p.push_back(make_pos(1, 1));
+  int col = 1;
+  if (color == BLACK)
+    col = -1;
+  if (can_move_to(c, 0, col) == 0)
+  {
+    p.push_back(make_pos(0, col));
+    if (((col == 1 && pos.row == TWO) || (col == -1 && pos.row == SEVEN))
+          && can_move_to(c, 0, 2*col) == 0)
+      p.push_back(make_pos(0, 2*col));
+  }
+  if (can_move_to(c, -1, col) == 1)
+    p.push_back(make_pos(-1, col));
+  if (can_move_to(c, 1, col) == 1)
+    p.push_back(make_pos(1, col));
   return p;
 }
--- src/engine/pieces/rook.hxx
+++ src/engine/pieces/rook.hxx
@@ -4,22 +4,22 @@ std::vector<Position> Rook::get_possible_move(ChessBoard *c) const
 {
   std::vector<Position> p = std::vector<Position>();
   int i = 1;
-  while (can_move_to(c, i, 0) > -1)
+  while (can_move_to(c, i, 0) == 0)
     p.push_back(make_pos(i++, 0));
   if (can_move_to(c, i, 0) == 1)
     p.push_back(make_pos(i, 0));
   i = -1;
-  while (can_move_to(c, i, 0) > -1)
+  while (can_move_to(c, i, 0) == 0)
     p.push_back(make_pos(i--, 0));
   if (can_move_to(c, i, 0) == 1)
     p.push_back(make_pos(i, 0));
   i = 1;
-  while (can_move_to(c, 0, i) > -1)
+  while (can_move_to(c, 0, i) == 0)
     p.push_back(make_pos(0, i++));
   if (can_move_to(c, 0, i) == 1)
     p.push_back(make_pos(0, i));
   i = -1;
-  while (can_move_to(c, 0, i) > -1)
+  while (can_move_to(c, 0, i) == 0)
     p.push_back(make_pos(0, i--));
   if (can_move_to(c, 0, i) == 1)
     p.push_back(make_pos(0, i));
