250129038bc41c3467b65ae945acf624b9e1888b
Daniel Baud <daniel.baud@epita.fr>
2018-05-29 20:12:35 +0200

    [AI] removed writing to stdout

--- src/player/ai.cc
+++ src/player/ai.cc
@@ -11,7 +11,6 @@ int main(int argc, char **argv)
   std::vector<std::string> empty = std::vector<std::string>();
   plugin::ListenerAdapter ladapter = plugin::ListenerAdapter(empty);
   AI ai = AI(std::string(argv[1]), std::string(argv[2]), ladapter);
-  std::cout << (ai.get_color()?"BLACK":"WHITE") << std::endl;
   if (!ai.play())
     return 1;
   return 0;
